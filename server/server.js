ee8e118bfa0db0e79c9ac8bfcc5a3fb007bcf332
Justin Cathey <95rocketman@gmail.com>
2018-03-07 16:23:32 +0300

    added get todos/id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {TodoTemplate} = require('./models/todo');
 var {UserTemplate} = require('./models/user')
@@ -33,6 +33,30 @@ app.get('/todos', (req,res)=>{
 
 app.listen(3000,()=>{
   console.log("started on port 3000")
+});
+
+
+app.get('/todos/:id', (req,res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    console.log(`Problem with id:`, id);
+     return res.status(404).send('');
+  }
+  TodoTemplate.findById(id).then((todo)=>{
+    if(todo == null){
+      return res.status(404).send('');
+    }
+    res.send({todo});
+  },(er)=>{
+    res.status(400).send('');
+  });
+
+  // query findById
+    // success
+    // error  400 request not valid, send empty bodyParser
+
+  // if todo exists send it back
+  // if no todo send 404 empty body
 });
 
 module.exports = {app};
