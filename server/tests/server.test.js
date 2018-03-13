d67df950bd813365c8e410a591ba45741a37adda
Justin Cathey <95rocketman@gmail.com>
2018-03-13 16:46:48 +0300

    upgraded expect to 22.4.0 and made appropriate adjustments to code

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -117,7 +117,7 @@ describe('Delete /todos/delete/:id',()=>{
       .end((err,res)=>{
         if (err) return done(err);
         TodoTemplate.findById(tempID).then((todos)=>{
-          expect(todos).toBe(null);
+          expect(todos).toBeFalsy();
           done();
         }).catch((e)=>done(e));
       })
@@ -132,7 +132,7 @@ describe('Delete /todos/delete/:id',()=>{
       .end((err,res)=>{
         if (err) return done(err);
         TodoTemplate.findById(tempID).then((todos)=>{
-          expect(todos).toExist();
+          expect(todos).toBeTruthy();
           done();
         }).catch((e)=>done(e));
       })
@@ -171,7 +171,7 @@ describe('Patch /todos:id',()=>{
         expect(response.body.todo._id).toBe(id);
         expect(response.body.todo.text).toBe(texter);
         expect(response.body.todo.completed).toBe(true);
-        expect(response.body.todo.completedAt).toBeA('number');
+        expect(typeof response.body.todo.completedAt).toBe('number');
       })
       .end(done);
   });
@@ -200,7 +200,7 @@ describe('Patch /todos:id',()=>{
         expect(response.body.todo._id).toBe(id);
         expect(response.body.todo.text).toBe(texter);
         expect(response.body.todo.completed).toBe(false);
-        expect(response.body.todo.completedAt).toBe(null);
+        expect(response.body.todo.completedAt).toBeFalsy();
       })
       .end(done);
   });
@@ -241,15 +241,15 @@ describe('Post /users/add', ()=>{
       .send({email, password})
       .expect(200)
       .expect((res)=>{
-        expect(res.headers['x-auth']).toExist();
-        expect(res.body._id).toExist();
+        expect(res.headers['x-auth']).toBeTruthy();
+        expect(res.body._id).toBeTruthy();
         expect(res.body.email).toBe(email);
       })
       .end((err)=>{
         if(err) return done(err);
         UserTemplate.findOne({email}).then((user)=>{
-          expect(user).toExist();
-          expect(user.password).toNotBe(password);
+          expect(user).toBeTruthy();
+          expect(user.password).not.toBe(password);
           done();
         }).catch((e)=>done(e));
       });
@@ -288,12 +288,12 @@ describe('Post /users/login',()=>{
       })
       .expect(200)
       .expect((response)=>{
-        expect (response.headers['x-auth']).toExist();
+        expect (response.headers['x-auth']).toBeTruthy();
       })
       .end((err,res)=>{
         if (err) return done(err);
         UserTemplate.findById(preUsers[1]._id).then((user)=>{
-            expect(user.tokens[1]).toInclude({
+            expect(user.toObject().tokens[1]).toMatchObject({
               access:'auth',
               token: res.headers['x-auth']
             });
@@ -309,7 +309,7 @@ describe('Post /users/login',()=>{
       })
       .expect(400)
       .expect((response)=>{
-        expect (response.headers['x-auth']).toNotExist();
+        expect (response.headers['x-auth']).toBeFalsy();
       })
       .end((err,res)=>{
         if (err) return done(err);
